837ac3ac3fc564451b3503e816a31e3999012212
wslmacieira <wslmacieira@outlook.com>
2021-06-01 11:43:03 -0300

    criando header e footer template

--- config.php
+++ config.php
@@ -18,7 +18,7 @@ if ( !defined('ABSPATH') )
 	
 /** caminho no server para o sistema **/
 if ( !defined('BASEURL') )
-	define('BASEURL', '/crud-bootstrap-php/');
+	define('BASEURL', '/');
 	
 /** caminho do arquivo de banco de dados **/
 if ( !defined('DBAPI') )
